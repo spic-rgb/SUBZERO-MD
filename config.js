092d648b64798a91bb1804f4a2b44a417565f570
spic-rgb <chassupremo@gmail.com>
2025-05-08 00:45:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~v64lXLzC#mj5FSd_kTcFFVTmk5GPrtuDhcccc8FWwc4OBOSnviTQ",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD;;;wj9GWD7T#V785Z_l56H-GrAy-JGhSVsZFwOTafgHd4Rx5bOW8SLk",  // Your bot's session ID (keep it secure)
     PREFIX: process.env.PREFIX || ".",         // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || "SUBZERO-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
